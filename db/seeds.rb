2d6e5616f2739a5e77811cae32d4f6d80f4a0779
Nico <arrvac@gmail.com>
2020-11-05 00:13:23 +0100

    little bug login signup....

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 Event.destroy_all
 User.destroy_all
 
+User.create(first_name: "Nicolas", last_name: "Veron", email: "arrvac@yopmail.com", password: "password")
 10.times do
   User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email(domain: 'yopmail.com'), password: "password")
 end
